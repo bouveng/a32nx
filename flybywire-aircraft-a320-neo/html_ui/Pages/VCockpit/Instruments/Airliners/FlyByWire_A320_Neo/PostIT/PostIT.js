1a571f4e0b4649e3cccf6c28d4dfdb2dfc8aa04a
ErickSharp <titaninmc@gmail.com>
2021-12-21 05:58:29 -0600

    feat: more stuff

--- flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/PostIT/PostIT.js
+++ flybywire-aircraft-a320-neo/html_ui/Pages/VCockpit/Instruments/Airliners/FlyByWire_A320_Neo/PostIT/PostIT.js
@@ -13,35 +13,96 @@ class PostIT extends BaseInstrument {
 
     connectedCallback() {
         super.connectedCallback();
+
         this.textElem = document.getElementById("text");
+        this.invisElem = document.getElementById("invis");
+        this.taskbar = document.getElementById("taskbar");
+        this.typographyIcon = document.getElementById("typography");
+        this.paletteIcon = document.getElementById("palette");
+        this.pencilIcon = document.getElementById("pencil");
+
+        this.fonts = ['Caveat', 'Marker', 'Gochi', 'RockSalt', 'Kalam'];
+        // IMPORTANT NOTE: The formatting of these strings is very important. (They MUST be defined as 'rgb(xxx, xxx, xxx)')
+        this.penColors = ['rgb(0, 0, 0)', 'rgb(255, 0, 0)', 'rgb(22, 38, 76)', 'rgb(124, 51, 161)', 'rgb(212, 212, 212)'];
+        this.pageColors = ['rgb(255, 255, 151)', 'rgb(251, 174, 74)', 'rgb(234, 236, 64)', 'rgb(242, 117, 173)', 'rgb(121, 203, 197)', 'rgb(177, 177, 177)'];
+
+        this.textElem.innerText = NXDataStore.get("POSTIT_CONTENT", "Put your favorite text here : )");
 
         if (this.textElem) {
             this.initMainLoop();
         }
 
-        this.textElem.addEventListener("keydown", (e) => {
-            console.log(e.target.value);
-            NXDataStore.set("POSTIT_CONTENT", e.target.value);
+        if (NXDataStore.get("POSTIT_FONT")) {
+            this.textElem.style.fontFamily = NXDataStore.get("POSTIT_FONT");
+        }
+
+        if (NXDataStore.get("POSTIT_PAGE_COLOR")) {
+            this.textElem.style.backgroundColor = NXDataStore.get("POSTIT_PAGE_COLOR");
+            this.paletteIcon.style.stroke = NXDataStore.get("POSTIT_PAGE_COLOR");
+        }
+
+        if (NXDataStore.get("POSTIT_PEN_COLOR")) {
+            this.textElem.style.color = NXDataStore.get("POSTIT_PEN_COLOR");
+            this.pencilIcon.style.stroke = NXDataStore.get("POSTIT_PEN_COLOR");
+        }
+
+        const setProperties = (options, textElemProperty, iconElem, dataStoreKey) => {
+            this.textElem.focus();
+            const option = options[(options.indexOf(this.textElem.style[textElemProperty]) + 1) % options.length];
+            this.textElem.style[textElemProperty] = option;
+
+            if (iconElem) {
+                iconElem.style.stroke = option;
+            }
+
+            NXDataStore.set(dataStoreKey, option);
+        };
+
+        this.paletteIcon.addEventListener("click", () => {
+            setProperties(this.pageColors, "backgroundColor", this.paletteIcon, "POSTIT_PAGE_COLOR");
+        });
+
+        this.pencilIcon.addEventListener("click", () => {
+            setProperties(this.penColors, "color", this.pencilIcon, "POSTIT_PEN_COLOR");
+        });
+
+        this.typographyIcon.addEventListener("click", () => {
+            setProperties(this.fonts, "fontFamily", null, "POSTIT_FONT");
         });
 
         this.textElem.addEventListener("input", () => {
-            // The character below being replaced is U+007f (DELETE Character)
+            // The character being replaced is U+007f (DELETE Character)
             this.textElem.value = this.textElem.value.replace(/\u007f/g, '');
         });
 
+        this.textElem.addEventListener("keypress", (e) => {
+            // The character below is a one em-unit sized space
+            this.invisElem.innerText = e.target.value + "\u2003";
+
+            if (this.invisElem.clientHeight > 1000) {
+                e.preventDefault();
+            }
+        });
+
         this.textElem.addEventListener("click", () => {
-            this.textFocused ? Coherent.trigger("UNFOCUS_INPUT_FIELD") : Coherent.trigger("FOCUS_INPUT_FIELD");
             this.textFocused = !this.textFocused;
 
+            this.taskbar.style.display = this.textFocused ? "flex" : "none";
+
             if (this.textFocused) {
+                Coherent.trigger("FOCUS_INPUT_FIELD");
+                this.textElem.focus();
+            } else {
+                Coherent.trigger("UNFOCUS_INPUT_FIELD");
+                this.textElem.blur();
+
                 setTimeout(() => {
                     Coherent.trigger("UNFOCUS_INPUT_FIELD");
                     this.textFocused = false;
                 }, 30000);
-            }
 
-            this.textFocused ? this.textElem.focus() : this.textElem.blur();
-            document.getElementById('outline').style.display = this.textFocused ? "block" : "none";
+                NXDataStore.set("POSTIT_CONTENT", this.textElem.value);
+            }
         });
     }
 
@@ -62,10 +123,6 @@ class PostIT extends BaseInstrument {
     }
 
     disconnectedCallback() {}
-
-    Update() {
-        this.textElem.innerText = NXDataStore.get("POSTIT_CONTENT") || "Put your favorite text here : )";
-    }
 }
 
-registerInstrument("livery-postit-element", PostIT);
+registerInstrument("postit-element", PostIT);
